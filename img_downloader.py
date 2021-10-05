b4ec4c61956c631bbe458e55b8ae8fe2e56895df
abhaychandra123 <71028688+abhaychandra123@users.noreply.github.com>
2021-10-05 22:08:18 +0530

    Update and rename WhatsApp_img_downloader.py to img_downloader.py

--- img_downloader.py
+++ img_downloader.py
@@ -1,43 +1,41 @@
 '''
-This is a whatsapp img downloader, with which you can download enormous num of images along with naming them.
-REQUIREMENT:
-1. Have an ide for python
-2. Intall the pyautogui library through terminal.Type pip install pyautogui
-Check https://pyautogui.readthedocs.io/en/latest/install.html if not working
-
-
+This is a whatsapp and Google drive img downloader, with which you can download enormous num of images along with naming them.
 INSTRUCTIONS:
 1. Count the num of images
 2.Goto start image and download to the desired folder.
 3. Go back to the img
 4. Now run this code, Enter the name preffix, start point(here 1) and end point( here num of images-1)
 5.Quickly switch back to the image and enjoy!
-PS Make sure there isn't any other type of media in the images
 '''
 import pyautogui, time
 pyautogui.FAILSAFE= True
 pre = input("Enter the preffix: ")
 start = int(input("Start from? "))
 end = int(input("End point? "))
+mode = input("Enter the mode(wp or dr): ")
+delay= float(input("Delay? "))
+
+
 print()
 print("Switch to page!!")
 time.sleep(5)
 for i in range(start, end + 1):
-    # pyautogui.rightClick(751, 339)
-    # pyautogui.click(794, 385)
-    pyautogui.rightClick(751,339)
-    pyautogui.press("down")
-    pyautogui.press("down")
-    pyautogui.press("enter")
 
-    time.sleep(1)
+    if mode=="wp":
+        pyautogui.rightClick(751,339)
+        pyautogui.press("down")
+        pyautogui.press("down")
+        pyautogui.press("enter")
+    elif mode=="dr":
+        pyautogui.hotkey('ctrl','s')
+    time.sleep(delay)
     pyautogui.typewrite(pre + str(i))
     time.sleep(0.75)
 
     # pyautogui.click(1287, 53)
     pyautogui.press("enter")
 
-    time.sleep(0.75)
+    time.sleep(1)
 
     # pyautogui.click(1316, 430)
     pyautogui.press("right")
